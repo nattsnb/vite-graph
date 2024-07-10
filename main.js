972b3c0ee17afdf51c0d37ed64677cc2f4b8cd06
Natasza ASzatan <natiye.nb@gmai.com>
2024-07-10 11:22:23 +0100

    created graph

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 import './style.css'
+import {Graph} from "./Graph.js";
+import {GraphNode} from "./GraphNode.js";
 
 console.log("Script is working")
+
+const firstNode = new GraphNode(1);
+
+const graph = new Graph(firstNode);
+
+const secondNode = graph.add(12, firstNode, null);
+const thirdNode = graph.add(31, secondNode, firstNode);
+const lastNode = graph.add(666, thirdNode, null);
+console.log(graph);
